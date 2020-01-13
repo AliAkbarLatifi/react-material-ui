4f7690fad0e12b827dfce7db692cefb0bf6f8a25
Ali Latifi <aal.soft@gmail.com>
2020-01-13 22:46:32 +0330

    Cleared the codes

--- src/containers/Router/Router.js
+++ src/containers/Router/Router.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from '../Home';
-import Questions from '../Questions';
 import NotFound from '../NotFound';
 
 function Router() {
@@ -11,7 +10,6 @@ function Router() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/questions" exact component={Questions} />
         <Route compponent={NotFound} />
       </Switch>
     </BrowserRouter>
